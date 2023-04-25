cc7de5796f623c23e041ba5bec4464f846957095
Stagiaire <dupratdonovan@gmail.com>
2023-04-25 16:29:46 +0200

    fin du TP1 et TP2

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -5,13 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/wish', name: 'wish_wishes')]
 class WishController extends AbstractController
 {
-    #[Route('/wish', name: 'wish_wishes')]
+    #[Route('/', name: 'wish_wishes')]
     public function wishes(): Response
     {
         return $this->render('wish/wishes.html.twig', [
+            'controller_name' => 'WishController',
+        ]);
+    }
+    #[Route('/list', name: 'wish_list')]
+    public function list(): Response
+    {
+        return $this->render('wish/list.html.twig', [
+            'controller_name' => 'WishController',
+        ]);
+    }
+    #[Route('/detail', name: 'wish_detail')]
+    public function detail(): Response
+    {
+        return $this->render('wish/detail.html.twig', [
             'controller_name' => 'WishController',
         ]);
     }
